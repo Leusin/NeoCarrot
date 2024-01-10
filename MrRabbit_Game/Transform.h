875a1775324e62f03d9d4aef47f7f0315686c93e
Leusin <leusin@naver.com>
2024-01-10 09:52:14 +0900

    Update Transform.h

--- MrRabbit_Game/Transform.h
+++ MrRabbit_Game/Transform.h
@@ -3,7 +3,8 @@
 
 #pragma once
 
-#include "../UnityLike_Core/IComponent.h"
+#include "IComponent.h"
+#include "Matrix.h"
 
 namespace game
 {
@@ -13,6 +14,29 @@ class Transform : public core::IComponent
 public:
     Transform();
     ~Transform();
+
+    /*
+    
+    	DirectX::XMMATRIX GetWorld();
+		DirectX::XMMATRIX GetView();
+		DirectX::XMMATRIX GetProj();
+		
+		void SetWorld(const DirectX::XMMATRIX& w);
+		void SetView(const DirectX::XMMATRIX& v);
+		void SetProj(const DirectX::XMMATRIX& p);
+
+		// world, view, proj 를 한꺼번에 초기화
+		void SetTM(const DirectX::XMMATRIX& world, const DirectX::XMMATRIX& view, const DirectX::XMMATRIX& proj);
+
+		DirectX::XMMATRIX WorldViewProj();
+		// world, view, proj 곱을 반환
+
+    */
+
+private:
+    me::Matrix _world;
+    me::Matrix _view;
+    me::Matrix _proj;
 };
 
 } // namespace game
