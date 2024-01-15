605673233371b931b929d2b2b77f9336a66ecfdd
Leusin <98079486+Leusin@users.noreply.github.com>
2024-01-15 23:07:04 +0900

    Revert "Add D3D11Context_mk2 class"
    This reverts commit 172bc79779ad95af8ac40f9c6348d04214cd3830.

--- NeoCarrot_Graphics/CameraPtr.cpp
+++ NeoCarrot_Graphics/CameraPtr.cpp
@@ -22,6 +22,12 @@ _camera(camera)
 
 void CameraPtr::Update(float dt)
 {
+
+#ifdef _DEBUG
+    std::cout << "\t\t\t\t\tCameraPtr Update\n";
+#endif // _DEBUG
+
+
     auto view = _camera->View();
     auto proj = _camera->Proj();
     auto eye  = _camera->GetPosition();
